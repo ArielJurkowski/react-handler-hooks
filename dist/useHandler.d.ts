5876beb1d35f20fb2facb500674e2463bfad6da0
ArielJurkowski <arieljurkowski@gmail.com>
2020-02-21 13:28:32 +0100

    🤦🏽‍♀️ Removed dist folder

--- dist/useHandler.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function useHandler(callback: () => any): () => any;
-export declare function useHandler<T1>(callback: (...args: [T1]) => any): (...args: [T1]) => any;
-export declare function useHandler<T1, T2>(callback: (...args: [T1, T2]) => any): (...args: [T1, T2]) => any;
-export declare function useHandler<T1, T2, T3>(callback: (...args: [T1, T2, T3]) => any): (...args: [T1, T2, T3]) => any;
-export declare function useHandler<T1, T2, T3, T4>(callback: (...args: [T1, T2, T3, T4]) => any): (...args: [T1, T2, T3, T4]) => any;
-export declare function useHandler<T1, T2, T3, T4, T5>(callback: (...args: [T1, T2, T3, T4, T5]) => any): (...args: [T1, T2, T3, T4, T5]) => any;
-export declare function useHandler<T1, T2, T3, T4, T5, T6>(callback: (...args: [T1, T2, T3, T4, T5, T6]) => any): (...args: [T1, T2, T3, T4, T5, T6]) => any;
-export declare function useHandler<T1, T2, T3, T4, T5, T6, T7>(callback: (...args: [T1, T2, T3, T4, T5, T6, T7]) => any): (...args: [T1, T2, T3, T4, T5, T6, T7]) => any;
